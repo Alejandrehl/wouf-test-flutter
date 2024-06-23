2b55f51175065eb8d14cc94c3e5d1dab976c5c55
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2024-06-22 22:21:56 -0400

    save

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => print("Get coordinates"),
+          child: const Text('Get coordinates'),
+        ),
+      ),
+    );
+  }
+}
